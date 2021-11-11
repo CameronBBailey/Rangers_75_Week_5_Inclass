d437f9788469ea6b190efe5a0e54f44c86bb3a80
CameronBBailey <CameronBradBailey@gmail.com>
2021-11-11 11:14:54 -0500

    first commit

---
+++ drone_inventory/site/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, render_template
+from flask_login.utils import login_required
+"""
+    Note that in the below code, some arguements are specified when creating Blueprint objects.
+    The first argument, 'site' is the blueprints name, which flask uses for routing.
+    
+    The second argument, __name__, is the Blueprint's import name, which flask uses to locate the Blueprint's resource
+
+"""
+
+site = Blueprint('site',__name__, template_folder = 'site_templates')
+@site.route('/')
+def home():
+    return render_template('index.html')
+
+@site.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html')
